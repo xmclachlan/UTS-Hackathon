bc2861bdc3447da80aed04600167052e7431e4cb
xmclachlan <xaviermclachlan1309@gmail.com>
2024-06-17 11:55:15 +1000

    Read out the first alarm and print out the Subsystem

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import pandas as pd
+import pdfplumber
+import re
+
+pdf_path = 'MAN_32-40_IMO_TierIII–Marine_.pdf'
 
 df = pd.read_excel("ALARMS.xlsx")
 
-print(df)
+first_subsystem = df['SUBSYSTEM'].iloc[0]
+print(f"First SUBSYSTEM value: {first_subsystem}")
