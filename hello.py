f1159b992c935282f4411839f081c9dc5e771e00
xmclachlan <xaviermclachlan1309@gmail.com>
2024-06-18 11:19:44 +1000

    Clean Flask Output

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template_string
 import subprocess
+import markdown
 
 app = Flask(__name__)
 
@@ -20,7 +21,15 @@ def run_script():
     user_input = request.form['input']
     result = subprocess.run(
         ['python3', 'query_data.py', user_input], capture_output=True, text=True)
-    return f'<pre>{result.stdout}</pre><pre>{result.stderr}</pre>'
+
+    # Convert the output to HTML using the markdown library
+    markdown_output = markdown.markdown(result.stdout)
+
+    # Render the HTML output
+    return render_template_string('''
+        <div>{{ output|safe }}</div>
+        <a href="/">Go back</a>
+    ''', output=markdown_output)
 
 
 if __name__ == '__main__':
